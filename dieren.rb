1de86a3b322f293f4fbd3baac56a3a90d6c28c1d
niek wijnen <niek.wijnen@hotmail.com>
2015-06-02 08:57:20 +0200

    first commit

---
+++ dieren.rb
@@ -0,0 +1,17 @@
+dieren = ["aap", "bever", "chimpansee", "drommedaris", "egel", "flamingo", "giraffe", "ijsvogel", "juffer", "kieviet", "leeuw", "mol", "neushoorn", "nijlpaard", "olifant", "pony", "rat", "slang", "tijger", "uil", "vleermuis", "walvis", "yak", "zebra"];
+
+
+puts "noem een dier"
+
+naam = gets.chomp
+
+puts naam.chars.last + " is de laatste letter." 
+
+for dier in dieren 
+
+	if dier.chars.first == naam.chars.last
+
+		puts dier		
+	end
+end
+
